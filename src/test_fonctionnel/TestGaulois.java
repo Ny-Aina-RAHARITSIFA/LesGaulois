3512d0ac57889ba340e9deeda7678ad3864c941b
Ny Aina <nyainamiradoraharitsifa1707@gmail.com>
2026-01-08 09:44:53 +0100

    TP1 4 question 9

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,12 +1,21 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Asterix",8);
 		System.out.println(asterix.getNom());
 		asterix.parler("Bonjour à tous");
+		
+		Romain minus = new Romain("Minus",6);
+		minus.parler("UN GAU… UN GAUGAU…");
+		
+		for (int i=0; i<2; i++) {
+			minus.recevoirCoup(3);
+		}
 	}
+	
 }
 
