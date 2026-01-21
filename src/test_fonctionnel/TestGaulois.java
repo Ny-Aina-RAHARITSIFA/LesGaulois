90ba5a6dd3f956c64fb0eb579119becb76b0525b
Ny Aina <nyainamiradoraharitsifa1707@gmail.com>
2026-01-21 08:51:34 +0100

    TP3 2 a 1

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,25 +1,13 @@
 package test_fonctionnel;
 
+import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Asterix",8);
-		System.out.println(asterix.getNom());
-		asterix.parler("Bonjour à tous");
-		
-		Romain minus = new Romain("Minus",6);
-		minus.parler("UN GAU… UN GAUGAU…");
-		
-		//for (int i=0; i<2; i++) {
-			//minus.recevoirCoup(3);
-		//}
-		for (int i=0; i<2; i++) {
-			asterix.frapper(minus);
-			minus.frapper(asterix);
-		}
-		asterix.frapper(minus);
+		Druide panoramix = new Druide ("Panoramix",0);
+		panoramix.fabriquerPotion(3);
 	}
 }
 
